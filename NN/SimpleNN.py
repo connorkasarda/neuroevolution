6efe2dda55ed6f988fa7f8cf346f4cc4f0c0b77c
Connor Kasarda <connorkasarda@gmail.com>
2022-10-17 12:40:37 -0600

    SimpleNN Class completed. SimpleNN creates 3-Layer neural network.

--- NN/SimpleNN.py
+++ NN/SimpleNN.py
@@ -1,11 +1,44 @@
+# Import necessary math packages
+import numpy, scipy.special
 # Constructs 3-layer (input, hidden, and output) neural network from scratch using OOP
 class SimpleNN:
     # Initializes neural network
-    def __init__():
-        pass
+    def __init__(self, inputs, hiddens, outputs, lrate):
+        # Sets number of input, hidden, and output nodes
+        self.inputs = inputs
+        self.hiddens = hiddens
+        self.outputs = outputs
+        # Links layers of nodes with initial, random-weighted connections
+        self.wih = numpy.random.normal(0.0, pow(self.inputs, -0.5), (self.hiddens, self.inputs))
+        self.who = numpy.random.normal(0.0, pow(self.hiddens, -0.5), (self.outputs, self.hiddens))
+        # Sets the learning rate
+        self.lrate = lrate
+        # Sets the activation function to the sigmoid function
+        self.afunc = lambda x: scipy.special.expit(x)
     # Trains the neural network
-    def train():
-        pass
+    def train(self, ilist, tlist):
+        # Converts input and target lists to 2D arrays
+        isignals = numpy.array(ilist, ndmin=2).T
+        tsignals = numpy.array(tlist, ndmin=2).T
+        # Computes signal values into hidden layer
+        hsignals = self.afunc(numpy.dot(self.wih, isignals))
+        # Computes signal values into output layer
+        osignals = self.afunc(numpy.dot(self.who, hsignals))
+        # Computes output layer signal error
+        oerrors = tsignals - osignals
+        # Computes hidden layer signal error
+        herrors = numpy.dot(self.who.T, oerrors)
+        # Updates the hidden-output weighted links based on computed errors
+        self.who += self.lrate * numpy.dot((oerrors * osignals * (1.0 - osignals)), numpy.transpose(hsignals))
+        # Updates the input-hidden weighted links based on computed errors
+        self.wih += self.lrate * numpy.dot((herrors * hsignals * (1.0 - hsignals)), numpy.transpose(isignals))
     # Queries the neural network
-    def query():
-        pass
+    def query(self, ilist):
+        # Converts input list to 2D array
+        isignals = numpy.array(ilist, ndmin=2).T
+        # Computes signal values into hidden layer
+        hsignals = self.afunc(numpy.dot(self.wih, isignals))
+        # Computes signal values into output layer
+        osignals = self.afunc(numpy.dot(self.who, hsignals))
+        # Returns the output signals as final output
+        return osignals
