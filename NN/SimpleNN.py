92c876fc6b8489eec23bd742bfdc0d681621a5a0
Connor Kasarda <connorkasarda@gmail.com>
2022-10-20 09:39:06 -0600

    Fixed up SimpleNN forward function.

--- NN/SimpleNN.py
+++ NN/SimpleNN.py
@@ -1,5 +1,5 @@
 """
-Builds and tests an n-layer neural network.
+Builds and tests a simple neural network.
 """
 # Adds math packages for neural network
 import numpy
@@ -15,8 +15,16 @@ class NeuralNetwork:
         # Adds a lambda function that computes the sigmoid activation function
         self.activatefunc = lambda x: 1 / (1 + numpy.exp(-x))
     # Performs forward propagation to query the network
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        # Convert horizontal inputs list into a vertical list and use to initialize outputs
+        outputs = numpy.array(inputs, ndmin=2).T
+        # Propogate over each matrix of links, computing signals
+        for layer in range(1, len(self.nodes)):
+            inputs = outputs
+            recieved = numpy.dot(self.links[layer - 1], inputs)
+            outputs = self.activatefunc(recieved)
+        # Return the prediction
+        return outputs
     # Performs backward propagation to train the network
     def backward(self):
         pass
