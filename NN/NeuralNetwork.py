ebf25232820ac63ec1f55137592ab7c8933e0d11
Connor Kasarda <connorkasarda@gmail.com>
2022-10-21 21:43:17 -0600

    Refitted NeuralNetwork as a class for a 3-layer neural network. Utilized lambda functions more.

--- NN/NeuralNetwork.py
+++ NN/NeuralNetwork.py
@@ -1,31 +1,44 @@
 """
-Builds and tests a simple neural network.
+Builds and tests a simple 3-layer (input, hidden, output) neural network.
 """
 # Adds math packages for neural network
 import numpy
 # Blueprint for n-layer neural network
 class NeuralNetwork:
     # Neural Network constructor
-    def __init__(self, nodes, learnrate) -> None:
-        # Sets the list that represents number of nodes per layer as well as learning rate
-        self.nodes = nodes
+    def __init__(self, inputs: int, hiddens: int, outputs: int, learnrate: float) -> None:
+        # Sets number of nodes for each layer and learning rate
+        self.inputs = inputs
+        self.hiddens = hiddens
+        self.outputs = outputs
         self.learnrate = learnrate
-        # Creates randomly weighted links between each pair of layers based on the Gaussian distribution
-        self.web = [numpy.random.normal(0.0, pow(self.nodes[layer - 1], -0.5), (self.nodes[layer], self.nodes[layer - 1])) for layer in range(1, len(self.nodes))]
-        # Adds a lambda function that computes the sigmoid activation function
-        self.activatefunc = lambda x: 1 / (1 + numpy.exp(-x))
+        # Declares lambda functions that compute weight matrices, activation signals, verticalizations of lists, and weight updates
+        self.connect = lambda x, y: numpy.random.normal(scale=pow(x, -0.5), size=(y, x))
+        self.activation = lambda x: 1 / (1 + numpy.exp(-x))
+        self.verticalize = lambda x: numpy.array(x, ndmin=2).T
+        self.update = lambda x, y, z: self.learnrate * numpy.dot((x * y * (1.0 - y)), numpy.transpose(z))
+        # Initialize weight matrices to connect the gaps between each pair of layers
+        self.innerweb = self.connect(inputs, hiddens)
+        self.outerweb = self.connect(hiddens, outputs)
     # Performs forward propagation to query the network
-    def query(self, inputs):
-        # Converts horizontal inputs list into a vertical list and use to initialize outputs
-        outputs = numpy.array(inputs, ndmin=2).T
-        signals = []
-        # Propogates over each matrix of links, computing signals
-        for level in self.web:
-            recieved = numpy.dot(level, outputs)
-            outputs = self.activatefunc(recieved)
-            signals.append(outputs)
-        # Returns the prediction
-        return signals
+    def query(self, inputs: list[float]) -> tuple:
+        # Converts inputs list into a vertical list
+        inputvalues = self.verticalize(inputs)
+        # Calculates hidden and outer level signal values
+        hiddenvalues = self.activation(numpy.dot(self.innerweb, inputvalues))
+        outputvalues = self.activation(numpy.dot(self.outerweb, hiddenvalues))
+        # Returns hidden and output values
+        return hiddenvalues, outputvalues
     # Performs backward propagation to train the network
-    def train(self, inputs, targets):
-        pass
+    def train(self, inputs: list[float], targets: list[float]) -> None:
+        # Computes a forward query on neural network
+        hiddenvalues, outputvalues = self.query(inputs)
+        # Converts inputs and targets lists to vertical lists
+        inputvalues = self.verticalize(inputs)
+        targetvalues = self.verticalize(targets)
+        # Calculates hidden and output errors
+        outputerrors = targetvalues - outputvalues
+        hiddenerrors = numpy.dot(self.outerweb.T, outputerrors)
+        # Updates inner and outer weights
+        self.outerweb += self.update(outputerrors, outputvalues, hiddenvalues)
+        self.innerweb += self.update(hiddenerrors, hiddenvalues, inputvalues)
