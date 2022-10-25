55985e81f735e9cb48fea1dae030711812c0f48f
Connor Kasarda <connorkasarda@gmail.com>
2022-10-24 21:55:57 -0600

    Added query and predict functions to ANN. The query functions returns outputs from all layers in a list whereas predict only returns output from last layer (otherwise known as the prediction).

--- DeepLearning/Artificial.py
+++ DeepLearning/Artificial.py
@@ -1,5 +1,6 @@
 """
 Purpose: Learn about deep learning by building an artificial neural network (can have more than 1 hidden layer) from scratch.
+         Instead of layers of nodes, represented as layers of "webs" or matrices
 """
 # Adds math packages for neural network
 import numpy, scipy.special
@@ -15,10 +16,8 @@ class Web:
         self.threads = numpy.random.normal(0.0, pow(parents, -0.5), (children, parents))
     # Computes a forward of the web, likely for a query
     def send(self, inputs):
-        # Converts inputs list into a vertical list
-        data = numpy.array(inputs, ndmin=2).T
         # Calculates output of the web
-        signals = numpy.dot(self.threads, data)
+        signals = numpy.dot(self.threads, inputs)
         activations = scipy.special.expit(signals)
         # Returns the output of the web
         return activations
@@ -34,10 +33,31 @@ class Artificial:
         self.nodes = nodes
         self.alpha = alpha
         # Webs are declared for each gap within neural network
-        self.webs = [Web(self.nodes[layer], self.nodes[layer + 1], self.alpha) for layer in range(len(self.nodes) - 2)]
+        self.webs = [Web(self.nodes[layer], self.nodes[layer + 1], self.alpha) for layer in range(len(self.nodes) - 1)]
+        # Sets up lambda functions
+        self.verticalize = lambda a: numpy.array(a, ndmin=2).T
     # Performs forward propagation for prediction
     def query(self, inputs):
-        pass
+        # Converts inputs list into a vertical list
+        data = self.verticalize(inputs)
+        # Begins list of predictions with input
+        predictions = []
+        predictions.append(data)
+        # Loops over each web until output layer is reached
+        for web in self.webs:
+            data = web.send(data)
+            predictions.append(data)
+        # Returns final output as result of query
+        return predictions
+    # Similar to query function, but only used for return output layer prediction
+    def predict(self, inputs):
+        # Converts inputs list into a vertical list
+        prediction = self.verticalize(inputs)
+        # Loops over each web until output layer is reached
+        for web in self.webs:
+            prediction = web.send(prediction)
+        # Returns final output as result of query
+        return prediction
     # Performs backward propagation for weight adjustments
     def train(self, inputs, targets):
         pass
