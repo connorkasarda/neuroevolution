85b4177c432fc16331b3083964c71a4d52935765
Connor Kasarda <connorkasarda@gmail.com>
2022-10-25 15:57:22 -0600

    Shortened train function in Artificial class.

--- DeepLearning/Artificial.py
+++ DeepLearning/Artificial.py
@@ -5,7 +5,7 @@ Purpose: Learn about deep learning by building an artificial neural network from
 """
 # Adds math packages for neural network
 import numpy, scipy.special
-# Blueprint for each layer in the neural network
+# Blueprint for each layer ("Web" or matrix of weighted connections) in the neural network
 class Web:
     # Web matrix constructor, where parents are nodes from previous layer and children are nodes from current layer
     def __init__(self, parents, children, alpha):
@@ -22,6 +22,7 @@ class Web:
         activations = scipy.special.expit(signals)
         # Returns the output of the web
         return activations
+    # Updates the weights of the matrix (threads) that represents the connections
     def update(self, errors, outers, inners):
         # Calculates change in weights
         delta = self.alpha * numpy.dot((errors * outers * (1.0 - outers)), numpy.transpose(inners))
@@ -37,7 +38,7 @@ class Artificial:
         self.webs = [Web(self.nodes[layer], self.nodes[layer + 1], self.alpha) for layer in range(len(self.nodes) - 1)]
         # Sets up lambda functions
         self.verticalize = lambda a: numpy.array(a, ndmin=2).T
-    # Performs forward propagation for prediction
+    # Performs forward propagation for predictions of all layers in neural network
     def query(self, inputs):
         # Converts inputs list into a vertical list
         data = self.verticalize(inputs)
@@ -50,7 +51,7 @@ class Artificial:
             predictions.append(data)
         # Returns final output as result of query
         return predictions
-    # Similar to query function, but only used for return output layer prediction
+    # Similar to query function, but only used to return the output layer prediction only
     def predict(self, inputs):
         # Converts inputs list into a vertical list
         prediction = self.verticalize(inputs)
@@ -65,11 +66,10 @@ class Artificial:
         query = self.query(inputs)
         # Converts targets list into a vertical list
         targets = self.verticalize(targets)
-        # Iterates over each web, calculating errors and updating weights
+        # Adjusts outermost weights with errors
         errors = targets - query[-1]
         self.webs[-1].update(errors, query[-1], query[-2])
-        qiter = -2
-        for witer in range(len(self.webs) - 1, 0, -1):
-            errors = numpy.dot(self.webs[witer].threads.T, errors)
-            self.webs[witer - 1].update(errors, query[qiter], query[qiter - 1])
-            qiter -= 1
+        # Iterates over remaining weights, calculating error differently and updating weight values
+        for iter in range(len(self.webs) - 1, 0, -1):
+            errors = numpy.dot(self.webs[iter].threads.T, errors)
+            self.webs[iter - 1].update(errors, query[iter], query[iter - 1])
